19c5bc122533c27609fd5df309ed8f95c939ce59
Erdodif <erdodif@gmail.com>
2021-10-13 23:54:13 +0200

    Semmi lényeges változtatás

--- app/src/main/java/com/example/quizion/MainActivity.kt
+++ app/src/main/java/com/example/quizion/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.quizion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.quizion.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
+
+    fun APIhivas(){
+        // TODO: 2021. 10. 13.  
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val bind : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        bind.buttonTempApi?.setOnLongClickListener{
+            APIhivas()
+            true
+        }
     }
 }
