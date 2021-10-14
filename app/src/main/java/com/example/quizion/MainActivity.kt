38237df5dcd0ddb61b919e1fb72e75d58652d2b5
Erdodif <erdodif@gmail.com>
2021-10-14 19:39:53 +0200

    Semmi lényeges változtatás

--- app/src/main/java/com/example/quizion/MainActivity.kt
+++ app/src/main/java/com/example/quizion/MainActivity.kt
@@ -12,7 +12,24 @@ import com.example.quizion.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     fun APIhivas(){
-        // TODO: 2021. 10. 13.  
+        // ...
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://10.147.20.1/quizion/adatok/index.php?method=listaz&tabla=quiz";
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            { response ->
+                // Display the first 500 characters of the response string.
+                Toast.makeText(this,
+                    "Response is: ${response.substring(0, 500)}", Toast.LENGTH_SHORT).show()
+            },
+            { Toast.makeText(this,
+                "Ez baj", Toast.LENGTH_SHORT).show() })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
