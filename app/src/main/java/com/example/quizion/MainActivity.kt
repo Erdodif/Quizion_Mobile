d365abb404f9f5b85126966229976206f0bbe904
Erdodif <erdodif@gmail.com>
2021-10-16 23:34:38 +0200

    http kérés végzése, SSLHandshakeException

--- app/src/main/java/com/example/quizion/MainActivity.kt
+++ app/src/main/java/com/example/quizion/MainActivity.kt
@@ -1,44 +1,55 @@
 package com.example.quizion
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.*
 import com.example.quizion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    fun APIhivas(){
-        // ...
-
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://10.147.20.1/quizion/adatok/index.php?method=listaz&tabla=quiz";
-
-        // Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            { response ->
-                // Display the first 500 characters of the response string.
-                Toast.makeText(this,
-                    "Response is: ${response.substring(0, 500)}", Toast.LENGTH_SHORT).show()
-            },
-            { Toast.makeText(this,
-                "Ez baj", Toast.LENGTH_SHORT).show() })
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val bind : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val bind: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        bind.buttonTempApi?.setOnLongClickListener{
-            APIhivas()
-            true
-        }
+        APIhivas(bind)
     }
+
+    fun APIhivas(bind: ActivityMainBinding) {
+        // Instantiate the cache
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        val network = BasicNetwork(HurlStack())
+
+        // Instantiate the RequestQueue with the cache and network. Start the queue.
+        val requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+
+        val url = "https://10.147.20.1/quizion/adatok/index.php?method=read&table=quiz";
+
+        // Formulate the request and handle the response.
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            { response ->
+                val ki = "Response is: " + response.substring(0, 500);
+                bind.textViewKerdes?.setText(ki)
+                Log.d("Eredmény:", ki)
+
+            },
+            { error ->
+                val ki = "Ez baj, $error"
+                bind.textViewKerdes?.setText(ki)
+                Log.d("Hiba: ", ki)
+            }
+        )
+        requestQueue.add(stringRequest)
+    }
+
 }
