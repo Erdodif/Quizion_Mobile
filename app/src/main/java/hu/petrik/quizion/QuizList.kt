40846c8eb0fdde750b17a41067403094189eeeeb
Erdodif <erdodif@gmail.com>
2021-11-09 09:57:33 +0100

    Kvízek nézet teszt label elkészítése (TODO: programatic generation)

--- app/src/main/java/hu/petrik/quizion/QuizList.kt
+++ app/src/main/java/hu/petrik/quizion/QuizList.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import hu.petrik.quizion.elemek.Quiz
 import androidx.appcompat.app.AppCompatActivity
 import hu.petrik.quizion.databinding.ActivityQuizListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class QuizList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +15,30 @@ class QuizList : AppCompatActivity() {
         val bind = ActivityQuizListBinding.inflate(layoutInflater)
         setContentView(bind.root)
         //ideiglenes
-        val intent = Intent(this,MainActivity::class.java)
+        /*val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
-        finish()
+        finish()*/
+        kiirat()
+    }
+
+    fun kiirat() = runBlocking{
+        val quizTolt = launch{
+            val kvizek = Quiz.getAll()
+            try {
+                if (kvizek!!.isNotEmpty()) {
+                    gombBetolt(kvizek)
+                } else {
+                    gombBetolt("Nem sikerült csatlakozni, vagy az adatot kinyerni...")
+                }
+            } catch (e: Exception) {
+                gombBetolt(e.message.toString())
+            }
+        }
+    }
+    fun gombBetolt(tartalom :List<Quiz>){
+
+    }
+    fun gombBetolt(tartalom : String){
+
     }
 }
