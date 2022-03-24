d0dcaf50bf2b8b8f42fc8ce8bf7895feb483973c
Erdodif <erdodif@gmail.com>
2022-03-24 23:50:07 +0100

    konstruktor túlterhelés, létrehozás azonnali animációval

--- app/src/main/java/hu/petrik/quizion/fragments/LoadingFragment.kt
+++ app/src/main/java/hu/petrik/quizion/fragments/LoadingFragment.kt
@@ -7,35 +7,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RawRes
 import com.airbnb.lottie.LottieAnimationView
 import hu.petrik.quizion.R
 import hu.petrik.quizion.databinding.FragmentLoadingBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass.
  * Use the [LoadingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoadingFragment : Fragment() {
-    lateinit var binding:FragmentLoadingBinding
+class LoadingFragment(@RawRes var res: Int? = null) : Fragment() {
+    lateinit var binding: FragmentLoadingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoadingBinding.inflate(layoutInflater)
+        if (res !== null) {
+            binding.lottieAnimationView.setAnimation(res!!)
+        }
         return binding.root
     }
 
-    fun setErrorMessage(errorMessage:String){
+    fun setErrorMessage(errorMessage: String) {
         binding.lottieAnimationView.visibility = LottieAnimationView.GONE
         binding.messageError.visibility = TextView.VISIBLE
         binding.messageError.text = errorMessage
         Log.d("Hiba", errorMessage)
     }
 
-    fun setAnimationSource(@RawRes res:Int){
+    fun setAnimationSource(@RawRes res: Int) {
         binding.lottieAnimationView.setAnimation(res)
     }
 }
